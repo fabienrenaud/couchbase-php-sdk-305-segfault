3d24692bfe4190aecd67868733b60b65b590b304
Fabien Renaud <fabienrenaud.fr@gmail.com>
2021-01-21 15:56:25 -0800

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -x
 
+network=$(docker inspect -f '{{.HostConfig.NetworkMode}}' $(docker-compose ps -q cb))
 docker build -t cb_segfault_repro_bug .
-docker run -it -v $(pwd):/app -w /app --network=cb_segfault_repro_default cb_segfault_repro_bug sh
+docker run -it -v $(pwd):/app -w /app --network=${network} cb_segfault_repro_bug sh
